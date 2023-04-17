7593640647934e4c347a84d484b46aca658e3e85
matthewadrian <matthewadrian08@berkeley.edu>
2023-04-17 12:07:44 -0700

    finished hw

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+  // Add more fields for information on the user.
+});
+
+// export model user with UserSchema
+module.exports = mongoose.model("user", UserSchema);
